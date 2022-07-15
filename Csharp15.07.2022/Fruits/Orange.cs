402d6f04656b8057cd634e84230784c165ae2912
4-1 <4-1@DESKTOP-ENM2TKM>
2022-07-15 21:13:38 +0500

    Добавьте файлы проекта.

---
+++ Csharp15.07.2022/Fruits/Orange.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp15._07._2022.Fruits
+{
+    class Orange
+    {
+        public Orange(string name, int weight)
+        {
+            Name = name;
+            Weight = weight;
+        }
+
+        public Orange()
+        {
+
+        }
+
+        public string Name { set; get; }
+
+        public int Weight { set; get; }
+    }
+}
---
+++ Csharp15.07.2022/Program.cs
@@ -0,0 +1,66 @@
+﻿using Csharp15._07._2022.Fruits;
+using System;
+using System.Linq;
+
+namespace Csharp15._07._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* Orange orange = new Orange("Самый вкусный апельсин", 15);
+
+             Orange orange1 = new() { Name = "Апельсин ", Weight = 15 };
+
+             Orange blue2 = new Orange();
+
+             String a = "Хай!";
+
+             string b = "Всем";
+
+             Console.BackgroundColor = ConsoleColor.Blue;
+
+             Console.WriteLine(a);
+             Console.Write(b);
+             string s = Console.ReadLine();
+             Console.WriteLine(s);
+
+             int c = int.Parse(Console.ReadLine());
+             Console.WriteLine(c);
+
+             Console.WriteLine("Hello World!");*/
+
+            //Задание 2
+            
+            int?[] arr = new int?[5];
+            int i = 0;
+            foreach (var item in arr)
+            {
+                Console.WriteLine($"Введите значения {i + 1} переменной: ");
+                string jj = Console.ReadLine();
+                if(jj == "")
+                {
+                    arr[i++] = 0;
+                    continue;
+                }
+                arr[i++] = int.Parse(jj);
+            }
+            Console.WriteLine(arr.Sum());
+            Console.WriteLine(arr.Aggregate((x, y) => x * y));
+            Console.WriteLine(arr.Max());
+            Console.WriteLine(arr.Min());
+
+            int? Mult(int?[] a)
+            {
+                int? i = 1;
+                foreach (var item in a)
+                {
+                    i = i * item;
+                }
+                return i;
+            }
+            Console.WriteLine(Mult(arr));
+        }
+    }
+}
